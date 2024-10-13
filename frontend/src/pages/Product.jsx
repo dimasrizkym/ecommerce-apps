fcd3dd1833734bb394ef628009f20510bb1dc855
Dimas Rizky M <dimasrizkym3@gmail.com>
2024-10-14 00:00:58 +0700

    feat: product detail

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,7 +1,213 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
-  return <div></div>;
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      {/* Product data */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Product image */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                key={index}
+                onClick={() => setImage(item)}
+                src={item}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+                alt=""
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img className="w-full h-auto" src={image} alt="" />
+          </div>
+        </div>
+        {/* Product details */}
+        <div className="flex-1">
+          <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src="" alt="" className="w-3 5" />
+            <img src="" alt="" className="w-3 5" />
+            <img src="" alt="" className="w-3 5" />
+            <img src="" alt="" className="w-3 5" />
+            <img src="" alt="" className="w-3 5" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency} {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  key={index}
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Product description and reviews */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            An e-commerce website is an online platform that facilitates the
+            buying and selling of products or services over the internet. It
+            serves as a virtual marketplace where businesses and individuals can
+            showcase their products, interact with customers, and conduct
+            transactions without the need for a physical presence. E-commerce
+            websites have gained immense popularity due to their convenience,
+            accessibility, and the global reach they offer.
+          </p>
+          <p>
+            E-commerce websites typically display products or services along
+            with detailed descriptions, images, prices, and any available
+            variations (e.g., sizes, colors). Each product usually has its own
+            dedicated page with relevant information.
+          </p>
+        </div>
+      </div>
+
+      {/* Related products */}
+      <div className="my-24">
+        <div className="text-center text-3xl py-2">
+          <div className="inline-flex gap-2 items-center mb-3">
+            <p className="text-gray-500">
+              RELATED{" "}
+              <span className="text-gray-700 font-medium">PRODUCTS</span>
+            </p>
+            <p className="w-8 sm:w-12 h-[1px] sm:h-[2px] bg-gray-700"></p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+          <a
+            className="text-gray-700 cursor-pointer"
+            href="/product/6683d5b67f779795ecfa98bb"
+          >
+            <div className="overflow-hidden">
+              <img
+                className="hover:scale-110 transition ease-in-out"
+                src="https://raw.githubusercontent.com/avinashdm/gs-images/main/forever/p_img14.png"
+                alt=""
+              />
+            </div>
+            <p className="pt-3 pb-1 text-sm">
+              Boy Round Neck Pure Cotton T-shirt
+            </p>
+            <p className="text-sm font-medium">$60</p>
+          </a>
+          <a
+            className="text-gray-700 cursor-pointer"
+            href="/product/6683d4b27f779795ecfa98ab"
+          >
+            <div className="overflow-hidden">
+              <img
+                className="hover:scale-110 transition ease-in-out"
+                src="https://raw.githubusercontent.com/avinashdm/gs-images/main/forever/p_img6.png"
+                alt=""
+              />
+            </div>
+            <p className="pt-3 pb-1 text-sm">Girls Round Neck Cotton Top</p>
+            <p className="text-sm font-medium">$56</p>
+          </a>
+          <a
+            className="text-gray-700 cursor-pointer"
+            href="/product/6683d7567f779795ecfa98cf"
+          >
+            <div className="overflow-hidden">
+              <img
+                className="hover:scale-110 transition ease-in-out"
+                src="https://raw.githubusercontent.com/avinashdm/gs-images/main/forever/p_img24.png"
+                alt=""
+              />
+            </div>
+            <p className="pt-3 pb-1 text-sm">
+              Boy Round Neck Pure Cotton T-shirt
+            </p>
+            <p className="text-sm font-medium">$30</p>
+          </a>
+          <a
+            className="text-gray-700 cursor-pointer"
+            href="/product/6683d7b57f779795ecfa98d5"
+          >
+            <div className="overflow-hidden">
+              <img
+                className="hover:scale-110 transition ease-in-out"
+                src="https://raw.githubusercontent.com/avinashdm/gs-images/main/forever/p_img3.png"
+                alt=""
+              />
+            </div>
+            <p className="pt-3 pb-1 text-sm">Girls Round Neck Cotton Top</p>
+            <p className="text-sm font-medium">$38</p>
+          </a>
+          <a
+            className="text-gray-700 cursor-pointer"
+            href="/product/6683d6547f779795ecfa98c3"
+          >
+            <div className="overflow-hidden">
+              <img
+                className="hover:scale-110 transition ease-in-out"
+                src="https://raw.githubusercontent.com/avinashdm/gs-images/main/forever/p_img18.png"
+                alt=""
+              />
+            </div>
+            <p className="pt-3 pb-1 text-sm">
+              Boy Round Neck Pure Cotton T-shirt
+            </p>
+            <p className="text-sm font-medium">$28</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 };
 
 export default Product;
